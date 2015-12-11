b74f7a75bbd5137716febdabb534bd68e01f9706
Maarten de Wolf <pilootwolfje@hotmail.com>
2015-12-11 09:36:49 +0100

    check

---
+++ Sprint 2/1018_NewsletterCheck/code.php
@@ -0,0 +1,12 @@
+<?php 
+
+$user = \App\User::where('id', $id)->first();
+$email = $user['hasnewsletter'];
+if ($email == 1) {
+	$email = true;
+	echo 'Your email is allready registered'
+} else {
+	$email = false;
+}
+
+?> 
